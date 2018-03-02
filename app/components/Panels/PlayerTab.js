8075d2a41255dea22591a2c698c6d6aa2dcf9c50
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2018-03-01 23:49:48 -0500

    Hid the value of the second turn while it's not the dealer's turn

--- app/components/Panels/PlayerTab.js
+++ app/components/Panels/PlayerTab.js
@@ -3,13 +3,36 @@ import React, { Component } from 'react';
 import styles from '../../styles/MainPage.css';
 
 class PlayerTab extends Component {
+
+	partialValue() {
+		if (this.props.dealer.currentTurn.cards.length > 0) {
+			if (
+				this.props.dealer.currentTurn.cards[0].charAt(0) == 'J' ||
+				this.props.dealer.currentTurn.cards[0].charAt(0) == 'Q' ||
+				this.props.dealer.currentTurn.cards[0].charAt(0) == 'K' ||
+				this.props.dealer.currentTurn.cards[0].charAt(0) == '1') {
+
+					return 10;
+				}
+				else if (this.props.dealer.currentTurn.cards[0].charAt(0) != 'A') {
+					return parseInt(this.props.dealer.currentTurn.cards[0].charAt(0));
+				}
+				else {
+					return 11;
+				}
+		}
+		else {
+			return 0;
+		}
+	}
+
 	render() {
 		return (
 			<div>
 				{
 					this.props.isDealer ? (
 						<div className={styles.PlayerTab}>
-							<div className={styles.PlayerTabTotalCount}>{this.props.dealer.currentTurn.currentValue}</div>
+							<div className={styles.PlayerTabTotalCount}>{this.partialValue()}</div>
 							<div className={styles.PlayerTabPlayerName}>{this.props.dealer.username}</div>
 						</div>
 					) : (
