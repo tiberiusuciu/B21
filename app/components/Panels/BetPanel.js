5555c0ad36735dd4986f4ff873c3dcdd324fba34
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2018-03-09 21:44:17 -0500

    Added money display

--- app/components/Panels/BetPanel.js
+++ app/components/Panels/BetPanel.js
@@ -9,6 +9,7 @@ class BetPanel extends Component {
 			currentTurn: {
 				currentBet: 0,
 			},
+			money: 100,
 		};
 		for (var i = 0; i < this.props.users.length; i++) {
 			if (this.props.users[i].id == id) {
@@ -27,6 +28,7 @@ class BetPanel extends Component {
 	render() {
 		return (
 			<div className={styles.BetPanel}>
+				<div className={styles.BetMoney}>Available Money: {this.findUser(this.props.userId).money} </div>
         <div className={styles.BetAnnouncement}>Current Bet: {this.findUser(this.props.userId).currentTurn.currentBet} </div>
         <div className={styles.BetOptions}>
           <div className={styles.PlayerBetButton} onClick={() => {this.handleBetClick(5)}}>5</div>
